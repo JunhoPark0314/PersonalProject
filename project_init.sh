88301a115acbb662c1af2f24c0eb101dbd1267f2
junho park <pjh3974@gmail.com>
2021-01-27 07:30:20 +0000

    project init bash update

--- project_init.sh
+++ project_init.sh
@@ -1,5 +1,5 @@
 GIT="https://github.com/"
-PROJECT_ROOT="/home/sysadmin/projects"
+PROJECT_ROOT=$(pwd)
 TORCH_VERSION=$(pip show torch | grep Version)
 CUDA_VERSION=$(ls /usr/local/ | grep "cuda-" | sed "s/cuda-//")
 COCO_PATH="datasets/coco"
@@ -20,41 +20,53 @@ fi
 
 cd ${PROJECT_ROOT}
 
-if [[ -z "fvcore" ]]; then
+if [[ ! -e "fvcore" ]]; then
 	git clone "${GIT}JunhoPark0314/fvcore.git"
+	$(GIT_FETCH "fvcore")
 	pip install -e fvcore
 fi
 
-$(GIT_FETCH "fvcore")
 
-if [[ -z "detectron2" ]]; then
+if [[ ! -e "detectron2" ]]; then
 	git clone "${GIT}JunhoPark0314/detectron2.git"
+	$(GIT_FETCH "detectron2")
 	pip install -e detectron2
 fi
 
-$(GIT_FETCH "detectron2")
 
-if [[ -z "AdelaiDet" ]]; then
+if [[ ! -e "AdelaiDet" ]]; then
 	git clone "${GIT}JunhoPark0314/AdelaiDet.git"
+	$(GIT_FETCH "AdelaiDet")
 	pip install -e AdelaiDet
 fi
 
-$(GIT_FETCH "AdelaiDet")
 
-if [[ -z "cocoapi" ]]; then
+if [[ ! -e "cocoapi" ]]; then
 	git clone "${GIT}JunhoPark0314/cocoapi.git"
+	$(GIT_FETCH "cocoapi")
 	cd cocoapi/PythonAPI
 	make install
 	cd ${PROJECT_ROOT}
 fi
 
-$(GIT_FETCH "cocoapi")
-
 mkdir -p /media/pjh3974/datasets
-if [[ -z "detectron2/$COCO_PATH" ]]; then
+mkdir -p /media/pjh3974/output/detectron2
+mkdir -p /media/pjh3974/output/AdelaiDet
+
+if [[ ! -e "detectron2/$COCO_PATH" ]]; then
 	ln -s "$MOUNT_PATH/$COCO_PATH" detectron2/$COCO_PATH 
 fi
 
-if [[ -z "AdelaiDet/$COCO_PATH" ]]; then
+if [[ ! -e "detectron2/output" ]]; then
+	ln -s "$MOUNT_PATH/output/detectron2" detectron2/output
+fi
+
+
+if [[ ! -e "AdelaiDet/$COCO_PATH" ]]; then
 	ln -s "$MOUNT_PATH/$COCO_PATH" AdelaiDet/$COCO_PATH 
 fi
+
+if [[ ! -e "AdelaiDet/output" ]]; then
+	ln -s "$MOUNT_PATH/output/AdelaiDet" AdelaiDet/output
+fi
+
